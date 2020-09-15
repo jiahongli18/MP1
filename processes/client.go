bb99cf12ab53c9b891de3cc67ea7812a82d5ca7a
Jiahong Li <jack@gofisherman.com>
2020-09-14 22:50:38 -0400

    finished

--- processes/client.go
+++ processes/client.go
@@ -10,28 +10,8 @@ import (
   "time"
   "math/rand"
   "strconv"
+  "sync"
 )
-
-func delay(min int, max int){
-	num := rand.Intn(max-min) +min
-	time.Sleep(time.Duration(num) * time.Millisecond)
-  fmt.Println("done") 
-}
-
-func fetchdelay()(int, int){
-  f, err := os.Open("../config.txt")
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  scanner := bufio.NewScanner(f)
-  scanner.Scan()
-  delays := strings.Fields(scanner.Text())
-  min_delay, _ := strconv.Atoi(delays[0])
-  max_delay, _ := strconv.Atoi(delays[1])
-  f.Close()
-  return min_delay, max_delay
-}
 
 func main() {
   
@@ -59,13 +39,8 @@ func main() {
     //find the associating host/port according to the user's desired destination #
     ip, port := fetchHostPort(destination)
     min_delay, max_delay := fetchdelay()
-    
-    // go delay(3000,5000)
-    unicast_send(destination, ip + ":" + port, message)
 
-    // listen for reply
-    // message, _ := bufio.NewReader(conn).ReadString('\n')
-    // fmt.Print("Message from server: "+message)
+    unicast_send(destination, ip + ":" + port, message, min_delay, max_delay)
   }
 }
 
@@ -106,23 +81,52 @@ func fetchHostPort(destination string) (string, string){
   return "nil","nil"
 }
 
+//Simulate network delay by adding an extra layer before sending the message via the TCP channel
+func delay(min int, max int, groupTest *sync.WaitGroup){
+	num := rand.Intn(max-min) +min
+	time.Sleep(time.Duration(num) * time.Millisecond)
+
+  //decrement value of waitgroup and relay the flow of execution back to main  
+  groupTest.Done()    
+}
+
+//parses the min and max delays from the config file
+func fetchdelay()(int, int){
+  f, err := os.Open("../config.txt")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  scanner := bufio.NewScanner(f)
+  scanner.Scan()
+  delays := strings.Fields(scanner.Text())
+  min_delay, _ := strconv.Atoi(delays[0])
+  max_delay, _ := strconv.Atoi(delays[1])
+  f.Close()
+  return min_delay, max_delay
+}
+
 //Sends message to the destination process
-func unicast_send(process string, destination string, message string) {
+func unicast_send(process string, destination string, message string, min_delay int, max_delay int) {
   //dial to the TCP server using net library
   conn, err := net.Dial("tcp", destination)
   if err != nil {
     fmt.Println(err)
     return
   }
+
+  fmt.Printf("Sent '%s' to process %s, system time is %s\n", message, process, time.Now().Format("Jan _2 15:04:05.000"))
+
+   //set delay
+    groupTest := new(sync.WaitGroup)
+    go delay(min_delay,max_delay,groupTest)
+ 
+    //Wait group is used to block the execution of code in the main thread until all goroutines are complete and waitgroup counter is decremented to 0
+    groupTest.Add(1)
+    groupTest.Wait()
   // send to socket
   fmt.Fprintf(conn, process + " " + message)
-	fmt.Printf("Sent '%s' to process %s , system time is %s\n", message, process, time.Now().Format("Jan _2 15:04:05.000"))
+	
 }
 
-//Simulate network delay by adding an extra layer before sending the message via the TCP channel
-func Delay(min int, max int) {
-	num := rand.Intn(max-min) + min
-	time.Sleep(time.Duration(num) * time.Millisecond)
-	return
-}
 
--- processes/server.go
+++ processes/server.go
@@ -5,30 +5,26 @@ import (
 "fmt"
 "net"
 "os"
-"strconv"
 "strings"
 "time"
 )
 
-//Delivers the message received from the source process
+//Delivers the message received from the source process and prints out message, source, and time
 func unicast_receive(source string, message string) {
-	fmt.Printf("Received %q from process %s, system time is %s\n", message, source, time.Now())
+	fmt.Printf("Received %q from process %s, system time is %s\n", message, source, time.Now().Format("Jan _2 15:04:05.000"))
 }
 
-var count = 0
+// var count = 0
 
 func handleConnection(c net.Conn) {
   for {
+    //read message and source from request
     netData, err := bufio.NewReader(c).ReadString('\n')
     if err != nil {
       fmt.Println(err)
       return
     }
     str := strings.TrimSpace(string(netData))
-    
-    if str == "STOP" {
-        break
-    }
 
     //parse str that source sent to split up source and message
     source := strings.Split(str, " ")[0]
@@ -36,8 +32,6 @@ func handleConnection(c net.Conn) {
     // message = strings.Replace(destination, " ", "", -1)
 
     unicast_receive(source, message)
-    counter := strconv.Itoa(count) + "\n"
-    c.Write([]byte(string(counter)))
     }
     c.Close()
 }
@@ -49,6 +43,7 @@ func main() {
     return
   }
 
+  //get port number from user input and listen in on that port for requests
   PORT := ":" + arguments[1]
   l, err := net.Listen("tcp4", PORT)
   if err != nil {
@@ -63,7 +58,8 @@ func main() {
       fmt.Println(err)
       return
     }
+
+    //goroutine for handling requests made to server
     go handleConnection(c)
-      count++
     }
 }
